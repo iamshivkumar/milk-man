3a3525306aed9ba7d7d5ea9b738f46c93c1096f4
Shivkumar Konade <shivkumar.konade@virtoustack.com>
2021-11-06 10:45:39 +0530

    updated

--- lib/ui/pages/orders/providers/orders_provider.dart
+++ lib/ui/pages/orders/providers/orders_provider.dart
@@ -1,6 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../../core/models/order.dart';
-import '../../../../core/models/order_params.dart';
 import '../../../../core/providers/repository_provider.dart';
 
 final ordersProvider = StreamProvider.family<List<Order>, DateTime>(
